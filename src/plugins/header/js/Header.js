430de001f2c31d448164eca2fea98c904b5d5d83
Ashit Samal <ashitsamal91@gmail.com>
2021-05-31 06:46:57 +0530

    link_fix_try

--- src/plugins/header/js/Header.js
+++ src/plugins/header/js/Header.js
@@ -6,10 +6,10 @@ function Header() {
            
             <ul className={styles.navigation}>
                 <li><a href="/"><i className="fa fa-home" aria-hidden="true"></i></a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="/resume">Resume</a></li>
-                <li><a href="/extra-curricular">Extra Curricular</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="resume">Resume</a></li>
+                <li><a href="extra-curricular">Extra Curricular</a></li>
+                <li><a href="contact">Contact</a></li>
             </ul>
         </header>
     )
