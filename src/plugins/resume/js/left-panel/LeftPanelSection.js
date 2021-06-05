729cac5a3349f9cfbd9fd573a1b3f1ddde93543f
Ashit Samal <ashitsamal91@gmail.com>
2021-06-05 18:12:02 +0530

    animation_added

--- src/plugins/resume/js/left-panel/LeftPanelSection.js
+++ src/plugins/resume/js/left-panel/LeftPanelSection.js
@@ -1,57 +1,74 @@
+import { useState } from 'react';
 import ProficiencyBar from '../../../../widgets/proficiency-bar/ProficiencyBar';
 import styles from '../Resume.module.scss';
 
 const LeftPanelSection = (props) => {
     const { sectionInfo } = props;
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const handleCollapse = (event) => {
+        setIsCollapsed(!isCollapsed);
+    }
 
     return (
         <div className={styles.leftOneSectionContainer}>
             {(sectionInfo.type !== "custom" || (sectionInfo.type === "custom" && props.children)) && (
-                <div className={styles.leftSectionHeader}>{sectionInfo.heading}</div>
-            )}
-            {sectionInfo.type === "custom" && (
-                <div className={styles.leftSectionBody}>
-                    {props.children}
+                <div className={styles.leftSectionHeader}>
+                    {sectionInfo.heading}
+                    {isCollapsed ? (
+                        <i className="fa fa-chevron-down" aria-hidden="true" onClick={handleCollapse}></i>
+                    ) : (
+                        <i className="fa fa-chevron-up" aria-hidden="true" onClick={handleCollapse}></i>
+                    )}
                 </div>
             )}
-            {sectionInfo.type === "bar" && (
-                <div className={styles.leftSectionBody}>
-                    {sectionInfo.list_data.map((list, index) => {
-                        return (
-                            <ProficiencyBar 
-                                listData={list} showLevel={true}
-                            />                            
-                        );
-                    })}
-                </div>
-            )}
-            {sectionInfo.type === "list" && (
-                <div className={styles.leftSectionBody}>
-                    <ul>
-                        {sectionInfo.list_data.map((list, index) => {
-                            return (
-                                <li key={index}>{list.label}</li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            )}
-            {sectionInfo.type === "href-list" && (
-                <div className={styles.leftSectionBody}>
-                    <ul>
-                        {sectionInfo.list_data.map((list, index) => {
-                            return (
-                                <li key={index}>
-                                    {list.link.text ? (
-                                        <>{list.label}<a href={list.link.url}>{list.link.text}</a></>
-                                    ) : (
-                                        <a href={list.link.url}>{list.label}</a>
-                                    )} 
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+            {!isCollapsed && (
+                <>
+                    {sectionInfo.type === "custom" && (
+                        <div className={styles.leftSectionBody}>
+                            {props.children}
+                        </div>
+                    )}
+                    {sectionInfo.type === "bar" && (
+                        <div className={styles.leftSectionBody}>
+                            {sectionInfo.list_data.map((list, index) => {
+                                return (
+                                    <ProficiencyBar 
+                                        listData={list} showLevel={true}
+                                    />                            
+                                );
+                            })}
+                        </div>
+                    )}
+                    {sectionInfo.type === "list" && (
+                        <div className={styles.leftSectionBody}>
+                            <ul>
+                                {sectionInfo.list_data.map((list, index) => {
+                                    return (
+                                        <li key={index}>{list.label}</li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    )}
+                    {sectionInfo.type === "href-list" && (
+                        <div className={styles.leftSectionBody}>
+                            <ul>
+                                {sectionInfo.list_data.map((list, index) => {
+                                    return (
+                                        <li key={index}>
+                                            {list.link.text ? (
+                                                <>{list.label}<a href={list.link.url}>{list.link.text}</a></>
+                                            ) : (
+                                                <a href={list.link.url}>{list.label}</a>
+                                            )} 
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
--- src/plugins/resume/js/right-panel/RightPanelSection.js
+++ src/plugins/resume/js/right-panel/RightPanelSection.js
@@ -1,73 +1,93 @@
-import ProficiencyBar from '../../../../widgets/proficiency-bar/ProficiencyBar';
+import { useState } from 'react';
 import styles from '../Resume.module.scss';
 
 const RightPanelSection = (props) => {
     const { sectionInfo } = props;
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const handleCollapse = (event) => {
+        setIsCollapsed(!isCollapsed);
+    }
 
     return (
         <div className={styles.rightOneSectionContainer}>
-            <div className={styles.rightSectionHeader}>{sectionInfo.heading}</div>
-            {sectionInfo.type === "work" && (
-                <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
-                    {sectionInfo.list_data.map((list, index) => {
-                        return (
-                            <>
-                                <div>{list.from} <br /> {list.to}</div>
-                                <div>
-                                    <div className={styles.companyName}>{list.label}</div>
-                                    <ul>
-                                        {list.highlights.map((point, index) => {
-                                            return (
-                                                <li key={index}>{point}</li>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
-                            </>
-                        );
-                    })}
-                </div>
-            )}
-            {sectionInfo.type === "basic" && (
-                <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
-                    {sectionInfo.list_data.map((list, index) => {
-                        return (
-                            <>
-                                <div>{list.label}</div>
-                                <div>{list.value}</div>
-                            </>                           
-                        );
-                    })}
-                </div>
-            )}
-            {sectionInfo.type === "declare" && (
-                <div className={styles.rightSectionBody}>
-                    {sectionInfo.list_data.map((list, index) => {
-                        return (
-                            <>
-                                <div>{list.declaration}</div>
-                                <div className={styles.signatureText}>{list.signature}</div>
-                            </>                           
-                        );
-                    })}
-                </div>
-            )}
-            {sectionInfo.type === "href-list" && (
-                <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
-                    <ul>
-                        {sectionInfo.list_data.map((list, index) => {
-                            return (
-                                <li key={index}>
-                                    {list.link.text ? (
-                                        <>{list.label}<a href={list.link.url}>{list.link.text}</a></>
-                                    ) : (
-                                        <a href={list.link.url}>{list.label}</a>
-                                    )} 
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
+            <div className={styles.rightSectionHeader}>
+                {sectionInfo.heading}
+                {sectionInfo.type !== "declare" && (
+                    <>
+                        {isCollapsed ? (
+                            <i className="fa fa-chevron-down" aria-hidden="true" onClick={handleCollapse}></i>
+                        ) : (
+                            <i className="fa fa-chevron-up" aria-hidden="true" onClick={handleCollapse}></i>
+                        )}
+                    </>
+                )}
+            </div>
+            {!isCollapsed && (
+                <>
+                    {sectionInfo.type === "work" && (
+                        <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
+                            {sectionInfo.list_data.map((list, index) => {
+                                return (
+                                    <>
+                                        <div>{list.from} <br /> {list.to}</div>
+                                        <div>
+                                            <div className={styles.companyName}>{list.label}</div>
+                                            <ul>
+                                                {list.highlights.map((point, index) => {
+                                                    return (
+                                                        <li key={index}>{point}</li>
+                                                    );
+                                                })}
+                                            </ul>
+                                        </div>
+                                    </>
+                                );
+                            })}
+                        </div>
+                    )}
+                    {sectionInfo.type === "basic" && (
+                        <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
+                            {sectionInfo.list_data.map((list, index) => {
+                                return (
+                                    <>
+                                        <div>{list.label}</div>
+                                        <div>{list.value}</div>
+                                    </>                           
+                                );
+                            })}
+                        </div>
+                    )}
+                    {sectionInfo.type === "declare" && (
+                        <div className={styles.rightSectionBody}>
+                            {sectionInfo.list_data.map((list, index) => {
+                                return (
+                                    <>
+                                        <div>{list.declaration}</div>
+                                        <div className={styles.signatureText}>{list.signature}</div>
+                                    </>                           
+                                );
+                            })}
+                        </div>
+                    )}
+                    {sectionInfo.type === "href-list" && (
+                        <div className={styles.rightSectionBody + ' ' + styles.rightPanelGrid}>
+                            <ul>
+                                {sectionInfo.list_data.map((list, index) => {
+                                    return (
+                                        <li key={index}>
+                                            {list.link.text ? (
+                                                <>{list.label}<a href={list.link.url}>{list.link.text}</a></>
+                                            ) : (
+                                                <a href={list.link.url}>{list.label}</a>
+                                            )} 
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
+                    )}
+                </>
             )}
         </div>
     );
